db0caf96364998f777a12ff305f288fe77f4b77b
JyZo <jjy3680@naver.com>
2024-09-11 03:56:45 +0900

    react-query cache

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -14,6 +14,9 @@ export const RQSuperHeroesPage = () => {
   const { isLoading, isError, data, error } = useQuery({
     queryKey: ["super-heroes"],
     queryFn: fetchSuperHeroes,
+    // gcTime: 10,            화면이 바뀌어도 캐시를 일정시간 가지고 있을 시간
+    // staleTime: 5 * 1000,   데이터의 변화가 많지않아 다시 가져올 필요가 없어 데이터 상태를 fresh로 지니고 있을 시간
+    refetchOnMount: true,
   });
 
   if (isLoading) {
