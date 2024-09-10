c3766df1120a83300dbd428cac1f1fdcacba630b
JyZo <jjy3680@naver.com>
2024-09-10 23:38:03 +0900

    react-query set & dev tools

--- src/App.js
+++ src/App.js
@@ -3,32 +3,48 @@ import { Routes, Route, Link } from "react-router-dom";
 import { HomePage } from "./components/Home.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <QueryClientProvider client={queryClient}>
+      <>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
-      <Routes>
-        <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
-        <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />}></Route>
-        <Route path="/" element={<HomePage />}></Route>
-      </Routes>
-    </>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
+          <Route
+            path="/rq-super-heroes"
+            element={<RQSuperHeroesPage />}
+          ></Route>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,35 @@
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 export const RQSuperHeroesPage = () => {
-  return <h2>RQSuperHeroes Page</h2>;
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["super-heroes"],
+    queryFn: fetchSuperHeroes,
+  });
+
+  if (isLoading) {
+    return <h2>Loading....</h2>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
